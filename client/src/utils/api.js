a97ca7ef29336563a8187a776978fbc0bd7c259b
Ryan Machan <rjm21@case.edu>
2018-03-29 20:17:14 -0400

    update auth route paths

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 
 export default {
   authenticate: function() {
-    return axios.get('/api/authenticate', {
+    return axios.get('/api/auth/authenticate', {
       withCredentials: true
     });
   },
   login: function(loginData) {
-    return axios.post('/api/login', loginData);
+    return axios.post('/api/auth/login', loginData);
   },
   register: function(userData) {
-    return axios.post('/api/register', userData);
+    return axios.post('/api/auth/register', userData);
   },
   logout: function() {
-    return axios.post('/api/logout', {
+    return axios.post('/api/auth/logout', {
       withCredentials: true
     });
   },
