16290ce792da82d4f5bf431e40d62bade7d74f75
Ryan Machan <rjm21@case.edu>
2018-03-29 20:17:03 -0400

    split up auth controller functions and routes

---
+++ controllers/authController.js
@@ -0,0 +1,61 @@
+var passport = require('../config/passport');
+
+module.exports = {
+  // Return data for single team
+
+  authenticate: (req, res, next) => {
+    if(req.user) {
+      return res.status(200).json({
+        user: req.user,
+        authenticated: true
+      });
+    } else {
+      return res.json({
+        authenticated: false
+      });
+    }
+  },
+
+  register: (req, res, next) => {
+    passport.authenticate('local-signup', function(err, user, info) {
+      if (err) {
+        return next(err); // will generate a 500 error
+      }
+      if (!user) {
+        return res.json(info);
+      }
+      res.status(200).json(user);
+    })(req, res, next);
+  },
+
+  login: (req, res, next) => {
+    passport.authenticate('local-login', function(err, user, info) {
+      if (err) { return next(err); }
+      if (!user) { 
+        return res.json(info);
+      }
+
+      req.login(user, function(err){
+        if(err){
+          return next(err);
+        }
+        req.session.save((err) => {
+          if (err) {
+            return next(err);
+          }
+          res.status(200).json({ success: true });
+        });
+      });
+    })(req, res, next);
+  },
+
+  logout: (req, res, next) => {
+    req.logout();
+    req.session.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).send('OK');
+    });
+  }
+};
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const teamRoutes = require('./team');
 const gameRoutes = require('./game');
 const scheduleRoutes = require('./schedule');
+const authRoutes = require('./auth');
 
 // mlb schedule routes
 router.use('/teams', teamRoutes);
 router.use('/game', gameRoutes);
 router.use('/schedule', scheduleRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
