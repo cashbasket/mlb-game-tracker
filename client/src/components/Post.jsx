efc1fa98ab94f95ebcb6db2550bdc31d15d88d5c
Ryan Machan <rjm21@case.edu>
2018-04-03 01:10:03 -0400

    update to work with dashboard

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -9,6 +9,7 @@ import { withUser } from '../services/withUser';
 import moment from 'moment';
 import Button from 'material-ui/Button';
 import Delete from 'material-ui-icons/DeleteForever';
+import Divider from'material-ui/Divider';
 import API from '../utils/api';
 import Avatar from 'material-ui/Avatar';
 
@@ -28,6 +29,13 @@ const styles = theme => ({
 });
 
 class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameDate: '',
+      gameTitle: ''
+    };
+  }
   htmlDecode = (input) => {
     var e = document.createElement('div');
     e.innerHTML = input;
@@ -37,43 +45,63 @@ class Post extends React.Component {
   deletePost = (postId) => {
     API.deletePost(postId)
       .then((res) => {
-        this.props.getPosts(this.props.postData.gameId);
+        this.props.getPosts(this.props.postData.gameId ? this.props.postData.gameId: '');
       });
   }
 
   render() {
-    const { classes, postData } = this.props;
+    const { classes, postData, dashboard } = this.props;
     const text = postData.postText;
     return (
       <li>
         <div>
-          <Paper className={classes.post}>     
-            <Row>
-              <Col md={2}>
-                <Link to={`/user/${postData.user.username}`}>
-                  <Avatar
-                    alt={postData.user.username}
-                    src={postData.user.gravatar}
-                    className={classes.avatar}
-                  />
-                </Link>
-              </Col>
-              <Col md={10}> 
-                <Typography variant="subheading">
+          {!dashboard ? (
+            <Paper className={classes.post}>     
+              <Row>
+                <Col md={2}>
                   <Link to={`/user/${postData.user.username}`}>
+                    <Avatar
+                      alt={postData.user.username}
+                      src={postData.user.gravatar}
+                      className={`img-fluid ${classes.avatar}`}
+                    />
+                  </Link>
+                </Col>
+                <Col md={10}>
+                  <Typography variant="subheading">
+                    <Link to={`/user/${postData.user.username}`}>
+                      <strong>{postData.user.username}</strong>
+                    </Link> said:
+                  </Typography>
+                  <Typography className={classes.postText} dangerouslySetInnerHTML={{ __html: this.htmlDecode(text) }} />
+                  <Typography><em>{moment(postData.postDate).format('M/D/YYYY, h:mm a')}</em></Typography>
+                  {postData.user.id == this.props.user.id && 
+                  <Button style={{float: 'right'}} size="small" onClick={() => this.deletePost(postData.id)}>
+                    <Delete/> Delete Post
+                  </Button>
+                  }
+                </Col>
+              </Row>
+            </Paper>
+          ) : (
+            <Row>
+              <Col md={12}> 
+                <Typography><em>{moment(postData.postDate).format('M/D/YYYY, h:mm a')}</em></Typography>
+                <Typography variant="subheading">
+                  <Link component="a" to={`/user/${postData.user.username}`} onClick={() => this.props.handleUserChange(postData.user.username)}>
                     <strong>{postData.user.username}</strong>
                   </Link> said:
                 </Typography>
                 <Typography className={classes.postText} dangerouslySetInnerHTML={{ __html: this.htmlDecode(text) }} />
-                <Typography><em>{moment(postData.postDate).format('M/D/YYYY, h:mm a')}</em></Typography>
-                {postData.user.id == this.props.user.id && 
+               
+                {this.props.user && postData.user.id == this.props.user.id && 
                   <Button style={{float: 'right'}} size="small" onClick={() => this.deletePost(postData.id)}>
-                    <Delete/> Delete Post
+                    <Delete/> Delete
                   </Button>
                 }
               </Col>
             </Row>
-          </Paper>
+          )}
         </div>
       </li>
     );}
