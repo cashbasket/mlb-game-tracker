5314e781f4119e5595d66f4ebaf6c5b4c53a5132
Ryan Machan <rjm21@case.edu>
2018-04-04 22:31:15 -0400

    write functions for updating account info

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,8 @@
 const passport = require('passport');
+const db = require('../models');
+const bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
-  // Return data for single team
-
   authenticate: (req, res, next) => {
     if(req.user) {
       return res.status(200).json({
@@ -47,6 +47,56 @@ module.exports = {
         });
       });
     })(req, res, next);
+  },
+
+  account: (req, res) => {
+    const userId = req.user ? req.user.id : null;
+    db.user.findOne({
+      where: {
+        id: userId
+      },
+      attributes: { 
+        exclude: ['token', 'tokenExpires']
+      }
+    }).then(dbAccount => res.json({
+      user: dbAccount
+    }))
+      .catch(err => res.status(400).json(err));
+  },
+
+  update: (req, res) => {
+    const userId = req.user ? req.user.id : null;
+    db.user.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(user => {
+      if(user && user.email === req.body.email && user.id !== req.user.id)
+        return res.json({message: 'That email address is already in use.'});
+      return db.user.update({
+        email: req.body.email,
+        description: req.body.description,
+        teamId: req.body.favoriteTeam
+      }, {
+        where: {
+          id: userId
+        }
+      });
+    }).then(user => {
+      if(req.body.password.length) {
+        return db.user.update({
+          password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(8), null, true),
+        }, {
+          where: {
+            id: userId
+          }
+        });
+      } else {
+        return res.status(200).json(user);
+      }
+    }).then(user => {
+      res.status(200).json(user);
+    }).catch(err => res.status(400).json(err));
   },
 
   logout: (req, res, next) => {
