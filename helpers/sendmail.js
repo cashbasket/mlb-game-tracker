a34b6ecd8162c72c7cc122395eff8dd13977e511
Ryan Machan <rjm21@case.edu>
2018-04-05 02:23:47 -0400

    update email text

--- helpers/sendmail.js
+++ helpers/sendmail.js
@@ -1,6 +1,6 @@
 var nodemailer = require('nodemailer');
 
-function sendMail(type, to, hostName, token) {
+const sendMail = (type, to, hostName, protocol, token) => {
   var subject, text;
   var smtpTransport = nodemailer.createTransport({
     service: 'gmail',
@@ -10,14 +10,28 @@ function sendMail(type, to, hostName, token) {
     }
   });
   if (type === 'reset') {
-    subject = 'Password Reset';
-    text = 'Hi, there!\n\n You are receiving this email because you (or someone else) has requested a password reset for your account.\n\n' + 'To complete the password reset process, please click on the link below, or paste it into your browser:\n\n' +
-		'https://' + hostName + '/reset/' + token + '\n\n' +
-		'If you did not request this email, please ignore it; your password will remain unchanged.\n';
+    subject = 'WENT YARD Password Reset';
+    text = `Hi, there!
+    
+You are receiving this email because you (or someone else, but hopefully not) has requested a password reset for your account.
+
+To complete the password reset process, please click on the link below, or paste it into your browser:
+${protocol}://${hostName}/reset/${token}
+
+NOTE: this link will only work for 24 hours. If you do not reset your password before then, the link will expire and you will have to request another email via the "Forgot Password" page.
+
+If you did not request this email, please ignore it; your password will remain unchanged if you do nothing.
+
+Thanks!
+
+- The WENT YARD Team`;
   } else if (type === 'confirm-reset') {
-    subject = 'Your Password has been changed';
-    text = 'Hi (again),\n\n' +
-		'We\'re just letting you know that the password for your account was successfully changed.\n';
+    subject = 'Your WENT YARD password has been updated';
+    text = `Hello from WENT YARD (again)!
+
+We're just letting you know that the password for your account was successfully updated... hopefully by you.  If you are NOT the one who updated your password, please reply to this email and let us know.
+
+- The WENT YARD Team`;
   }
   var mailOptions = {
     to: to,
@@ -26,7 +40,7 @@ function sendMail(type, to, hostName, token) {
     text: text
   };
   return smtpTransport.sendMail(mailOptions);
-}
+};
 
 module.exports = {
   sendMail : sendMail
