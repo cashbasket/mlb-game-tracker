9e5a93e17d020da2cb94aeaf448b403b9c644be7
Brian Cocherell <brian.cocherell@gmail.com>
2018-04-08 01:35:05 -0400

    displaying box score info for past games

--- controllers/msfController.js
+++ controllers/msfController.js
@@ -6,7 +6,7 @@ module.exports = {
   divRecord: function(req, res) {
     axios({
       method: 'get',
-      url: `/${req.query.season}/division_team_standings.json?teamstats=W,L,GB&team=${req.query.team}`,
+      url: `/${req.params.season}/division_team_standings.json?teamstats=W,L,GB&team=${req.params.team}`,
       baseURL: 'https://api.mysportsfeeds.com/v1.2/pull/mlb',
       auth: {
         username: process.env.MSF_USERNAME,
@@ -14,6 +14,20 @@ module.exports = {
       }
     })
       .then(dbTeam => res.json(dbTeam.data.divisionteamstandings.division.filter(division => division.teamentry)[0].teamentry))
+      .catch(err => res.status(400).json(err));
+  },
+  
+  game: function(req, res) {
+    axios({
+      method: 'get',
+      url: `/${req.params.season}/game_boxscore.json?gameid=${req.params.id}&teamstats=R,H,E&playerstats=none`,
+      baseURL: 'https://api.mysportsfeeds.com/v1.2/pull/mlb',
+      auth: {
+        username: process.env.MSF_USERNAME,
+        password: process.env.MSF_PW
+      }
+    })
+      .then(dbGame => res.json(dbGame.data))
       .catch(err => res.status(400).json(err));
   }
 };
--- routes/api/msf.js
+++ routes/api/msf.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const msfController = require('../../controllers/msfController');
 
-// Matches with "/api/teams"
-router.route('/record')
+// Matches with "/api/msf/record"
+router.route('/record/:season/:team')
   .get(msfController.divRecord);  
+
+// Matches with "/api/msf/game"
+router.route('/game/:season/:id')
+  .get(msfController.game);
 
 module.exports = router;
